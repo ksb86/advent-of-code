5fdef88b073bd782d77e9ea4a4b22fb922402309
Shane Barnwell <ksbarnwell@gmail.com>
2020-12-06 21:33:11 -0700

    5.2

--- 5/2a.js
+++ 5/2a.js
@@ -1,8 +1,45 @@
 const path = require('path');
-const { getInput, newLineString, validateAnswer } = require('../helpers');
+const { getInput, validateAnswer } = require('../helpers');
 
-const lines = getInput(path.resolve(__dirname, './input'), true);
+const lines = getInput(path.resolve(__dirname, './input'));
+const rows = [...Array(128).keys()];
+const cols = [...Array(8).keys()];
 
+const makeSeatId = (lineRow, lineCol) => {
+    return lineRow*8+lineCol;
+};
 
+const seatIds = lines.map(line => {
+    // find row with first 7 letters
+    const rowCode = line.substring(0, 7);
+    const [lineRow] = rowCode.split('').reduce((acc, instruction) => {
+        if (instruction === 'F') {
+            return acc.slice(0, acc.length/2);
+        } else {
+            return acc.slice(acc.length/2);
+        }
+    }, rows);
 
-// console.log(validateAnswer(, ));
+    // find column with last 3 letters
+    const colCode = line.substring(7);
+    const [lineCol] = colCode.split('').reduce((acc, instruction) => {
+        if (instruction === 'L') {
+            return acc.slice(0, acc.length/2);
+        } else {
+            return acc.slice(acc.length/2);
+        }
+    }, cols);
+
+    return makeSeatId(lineRow, lineCol);
+});
+
+let missingSeat;
+
+seatIds.sort((a,b) => a-b).forEach((element, i, array) => {
+    if (array[i+1] && element+1 !== array[i+1] ) {
+        missingSeat = element+1;
+    }
+});
+
+console.log({missingSeat});
+console.log(validateAnswer(653, missingSeat));
