8dcbd9709fa0c18ee1ec2abd983ef9f8f92577d9
Shane Barnwell <ksbarnwell@gmail.com>
2021-12-05 00:19:42 -0700

    5

---
+++ 2021/5/2a.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const lodash = require('lodash');
+const { getInput, validateAnswer } = require("../../helpers");
+
+let input = getInput(path.resolve(__dirname, "./input.txt"));
+let points = [];
+
+const lines = input.map(line => {
+    const [start, end] = line.split(' -> ');
+    const [x1, y1] = start.split(',');
+    const [x2, y2] = end.split(',');
+    return {
+        x1: Number(x1),
+        y1: Number(y1),
+        x2: Number(x2),
+        y2: Number(y2),
+    };
+});
+
+let dupCount = 0;
+
+lines.forEach(({x1, y1, x2, y2}) => {
+    if (x1 === x2) {
+        const start = Math.min(y1, y2);
+        const end = Math.max(y1, y2);
+        for (let y = start; y <= end; y++) {
+            points.push(`${x1}-${y}`);
+        }
+    } else if (y1 === y2) {
+        const start = Math.min(x1, x2);
+        const end = Math.max(x1, x2);
+        for (let x = start; x <= end; x++) {
+            points.push(`${x}-${y1}`);
+        }
+    } else {
+        const xArr = lodash.range(x1, x2 + (x1 > x2 ? -1 : 1));
+        const yArr = lodash.range(y1, y2 + (y1 > y2 ? -1 : 1));
+        xArr.forEach((x, i) => {
+            points.push(`${x}-${yArr[i]}`);
+        });
+    }
+});
+
+const uniquePoints = [];
+const duplicates = [];
+// inefficient, should find better way
+points.forEach(point => {
+    if (uniquePoints.includes(point) && !duplicates.includes(point)) {
+        duplicates.push(point);
+        dupCount++;
+    } else {
+        uniquePoints.push(point);
+    }
+});
+
+console.log({dupCount});
+console.log(validateAnswer(19081, dupCount));
