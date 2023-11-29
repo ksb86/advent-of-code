65eeda5f5896a339a1fe22e92e9922dfd5f35172
Shane B <ksbarnwell@gmail.com>
2023-11-29 00:13:05 -0700

    get input, write files

--- new.js
+++ new.js
@@ -1,20 +1,55 @@
+require('dotenv').config();
+const axios = require('axios');
 const colors = require('colors');
 const { mkdirSync, copyFileSync, constants, writeFileSync } = require('fs');
 const [a, b, day] = process.argv;
-if (!day) {
-    console.error(`What day?`.bold.red);
-    process.exit();
-}
-const year = (new Date()).getFullYear();
-const newDir = `${__dirname}/${year}/${day}`;
-mkdirSync(newDir, {recursive: true});
-try {
-    copyFileSync(`${__dirname}/template_solution.js`, `${newDir}/1a.js`, constants.COPYFILE_EXCL);
-    copyFileSync(`${__dirname}/template_solution.js`, `${newDir}/2a.js`, constants.COPYFILE_EXCL);
-    copyFileSync(`${__dirname}/template_input.txt`, `${newDir}/input.txt`, constants.COPYFILE_EXCL);
-    console.error(`Files created in ${`'${year}/${day}'`.bold}!`.green)
-} catch (error) {
-    if (error.code === 'EEXIST') {
-        console.error(`Files already exists in ${`'${year}/${day}'`.bold}`.red)
+
+async function run() {
+    if (!day) {
+        console.error(`What day?`.bold.red);
+        process.exit();
     }
+    const year = (new Date()).getFullYear();
+    // const year = 2022;
+    const newDir = `${__dirname}/${year}/${day}`;
+    mkdirSync(newDir, {recursive: true})
+
+    // get and write input file
+    try {
+        const { data } = await axios(`https://adventofcode.com/${year}/day/${day}/input`, {
+            headers: {
+                // copy session cookie from browser
+                Cookie: `session=${process.env.SESSION_COOKIE}`,
+            },
+        });
+        writeFileSync(`${newDir}/input.txt`, data);
+        console.log(`✅ Input retrieved and written to ${`'${year}/${day}/input.txt'`.bold}`.green);
+    } catch (error) {
+        try {
+            copyFileSync(`${__dirname}/template_input.txt`, `${newDir}/input.txt`, constants.COPYFILE_EXCL);
+            console.error(`❕ Unable to retrieve and write input, copied blank input template instead.`.yellow);
+        } catch (error2) {
+            if (error2.code === 'EEXIST') {
+                console.error(`❌ Input file already exists in ${`'${year}/${day}'`.bold}`.red);
+            } else {
+                console.error({error: error2});
+            }
+        }
+    }
+
+    // write solution files
+    try {
+        copyFileSync(`${__dirname}/template_solution.js`, `${newDir}/1a.js`, constants.COPYFILE_EXCL);
+        copyFileSync(`${__dirname}/template_solution.js`, `${newDir}/2a.js`, constants.COPYFILE_EXCL);
+        console.log(`✅ Solution files created in ${`'${year}/${day}'`.bold}`.green);
+    } catch (error) {
+        if (error.code === 'EEXIST') {
+            console.error(`❌ Solution files already exist in ${`'${year}/${day}'`.bold}`.red);
+        } else {
+            console.error({error});
+        }
+    }
+    console.log('\n');
 }
+console.log('\n');
+run();
