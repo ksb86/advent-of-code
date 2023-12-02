f6664a487e7b1afbf302ff46292270d4b4018baf
Shane B <ksbarnwell@gmail.com>
2023-12-02 00:09:11 -0700

    Open file on dev start

--- promptDev.js
+++ promptDev.js
@@ -16,6 +16,7 @@ const defaultDay = date.getDate()+1;
 rl.question(`Day (1-25) ? (default: ${defaultDay}) `, (day) => {
     rl.question('Part (1 or 2) (default: 1) ? ', (part) => {
         spawnProcess(`day=${day || defaultDay} part=${part || 1} yarn run-dev`);
+        spawnProcess(`code ${date.getFullYear()}/${day || defaultDay}/${part || 1}a.js`);
         rl.close();
     });
 });
