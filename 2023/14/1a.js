c180ad304c6b6c0ed70fba2ae8187114389608bf
Shane B <ksbarnwell@gmail.com>
2023-12-21 23:14:29 -0700

    clean

--- 2023/14/1a.js
+++ 2023/14/1a.js
@@ -9,11 +9,6 @@ let expectedOutput = 108759; // ACTUAL
 // input = getInput(path.resolve(__dirname, './example.txt'));
 // expectedOutput = 'tbd'; // EXAMPLE
 
-// console.log({input});
-// let index = ;
-let colCountTrack = Array(input.length).fill(0);
-let rockRowCounts = Array(input.length).fill(0);
-
 function transpose(a) {
     return Object.keys(a[0]).map(function(c) {
         return a.map(function(r) { return r[c]; }).join('');
